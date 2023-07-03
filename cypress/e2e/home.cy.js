a202dc3c4a720640010b5c61453a638bcb6f8048
kaspar-almayer <kaspar_almayer@protonmail.com>
2023-07-03 22:49:17 +0200

    update test

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,9 +1,9 @@
 describe('home page', () => {
   it('renders hero', () => {
     cy.visit('/')
-    cy.contains("Katalog niemieckich grobów w Polsce")
-    cy.contains('Verzeichnis deutscher Gräber in Polen').should('not.exist')
+    cy.contains("Niemieckie groby w Polsce")
+    cy.contains('Deutsche Gräber in Polen').should('not.exist')
     cy.get('.main-header__lang').get('select').select('de')
-    cy.contains('Verzeichnis deutscher Gräber in Polen').should('exist')
+    cy.contains('Deutsche Gräber in Polen').should('exist')
   })
 })
